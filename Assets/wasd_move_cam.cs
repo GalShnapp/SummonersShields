e42a2d3e32ecf97d02b2b3a4d37acc4e3a9ad59e
GalShnapp <gal.shnapp@gmail.com>
2024-03-15 19:28:43 +0200

    made the cam move, but square dissappears

--- Assets/wasd_move_cam.cs
+++ Assets/wasd_move_cam.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class wasd_move_cam : MonoBehaviour
 {
-
-    public GameObject camera;
-
+    public GameObject cam;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +12,17 @@ public class wasd_move_cam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        camera = GameObject.FindGameObjectsWithTag("MainCamera");
         if (Input.GetKeyDown(KeyCode.A)) {
             Debug.Log("A pressed");
             // camera.Transform.X(x+1)
-            camera.transform.
+            // camera.Transform.x = 3;
+            Vector3 currPos = cam.transform.position;
+            cam.transform.position = new Vector3(currPos.x + 3, currPos.y);
         }
         if (Input.GetKeyDown(KeyCode.D)) {
             Debug.Log("D pressed");
-            camera.Transform.x = -3;
+            Vector3 currPos = cam.transform.position;
+            cam.transform.position = new Vector3(currPos.x - 3, currPos.y);
         }
         if (Input.GetKeyDown(KeyCode.W)) {
             Debug.Log("W pressed");
