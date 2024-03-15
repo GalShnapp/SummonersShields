2d08903e51a1abee84ed9aa24eb845f4db8cca81
rony1philip <26e4u69@gmail.com>
2024-03-15 20:04:45 +0200

    with movement

--- Assets/wasd_move_cam.cs
+++ Assets/wasd_move_cam.cs
@@ -4,33 +4,35 @@ public class wasd_move_cam : MonoBehaviour
 {
     public GameObject cam;
 
-    private float moveSpeed = 1.5f;
+    private float moveSpeed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 currPos = cam.transform.position;
         if (Input.GetKeyDown(KeyCode.A)) {
             Debug.Log("A pressed");
             // camera.Transform.X(x+1)
-            // camera.Transform.x = 3;
-            Vector3 currPos = cam.transform.position;
+            // camera.Transform.x = 3
             cam.transform.position = new Vector3(currPos.x - this.moveSpeed, currPos.y, currPos.z);
         }
         if (Input.GetKeyDown(KeyCode.D)) {
             Debug.Log("D pressed");
-            Vector3 currPos = cam.transform.position;
             cam.transform.position = new Vector3(currPos.x + this.moveSpeed, currPos.y, currPos.z);
         }
         if (Input.GetKeyDown(KeyCode.W)) {
             Debug.Log("W pressed");
+            cam.transform.position = new Vector3(currPos.x , currPos.y + this.moveSpeed, currPos.z);
         }
         if (Input.GetKeyDown(KeyCode.S)) {
             Debug.Log("S pressed");
+            cam.transform.position = new Vector3(currPos.x , currPos.y - this.moveSpeed, currPos.z);
+
         }
     }
 }
