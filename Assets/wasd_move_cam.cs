0ffaacb46f6b665ec43ed5cd7153ed80c86d7b31
rony1philip <26e4u69@gmail.com>
2024-03-15 19:42:36 +0200

    fixed cam z

--- Assets/wasd_move_cam.cs
+++ Assets/wasd_move_cam.cs
@@ -17,12 +17,12 @@ public class wasd_move_cam : MonoBehaviour
             // camera.Transform.X(x+1)
             // camera.Transform.x = 3;
             Vector3 currPos = cam.transform.position;
-            cam.transform.position = new Vector3(currPos.x + 3, currPos.y);
+            cam.transform.position = new Vector3(currPos.x + 3, currPos.y, currPos.z);
         }
         if (Input.GetKeyDown(KeyCode.D)) {
             Debug.Log("D pressed");
             Vector3 currPos = cam.transform.position;
-            cam.transform.position = new Vector3(currPos.x - 3, currPos.y);
+            cam.transform.position = new Vector3(currPos.x - 3, currPos.y, currPos.z);
         }
         if (Input.GetKeyDown(KeyCode.W)) {
             Debug.Log("W pressed");
