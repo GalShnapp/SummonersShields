cd20ef9949c8f904299f151a115960c68a984254
GalShnapp <gal.shnapp@gmail.com>
2024-03-15 20:36:35 +0200

    move system (ish)

--- Assets/wasd_move_cam.cs
+++ Assets/wasd_move_cam.cs
@@ -4,7 +4,8 @@ public class wasd_move_cam : MonoBehaviour
 {
     public GameObject cam;
 
-    private float moveSpeed = 0.2f;
+    public float moveSpeed = 0.2f;
+    public float rotationSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,21 +16,34 @@ public class wasd_move_cam : MonoBehaviour
     void Update()
     {
         Vector3 currPos = cam.transform.position;
-        if (Input.GetKeyDown(KeyCode.A)) {
+        Quaternion currRot = this.transform.rotation;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.rotation = new Quaternion(currRot.x, currRot.y, currRot.z + this.rotationSpeed, currRot.w);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.rotation = new Quaternion(currRot.x, currRot.y, currRot.z - this.rotationSpeed, currRot.w);
+        }
+        if (Input.GetKey(KeyCode.A)) 
+        {
             Debug.Log("A pressed");
             // camera.Transform.X(x+1)
             // camera.Transform.x = 3
             cam.transform.position = new Vector3(currPos.x - this.moveSpeed, currPos.y, currPos.z);
         }
-        if (Input.GetKeyDown(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D)) 
+        {
             Debug.Log("D pressed");
             cam.transform.position = new Vector3(currPos.x + this.moveSpeed, currPos.y, currPos.z);
         }
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W)) 
+        {
             Debug.Log("W pressed");
             cam.transform.position = new Vector3(currPos.x , currPos.y + this.moveSpeed, currPos.z);
         }
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetKey(KeyCode.S)) 
+        {
             Debug.Log("S pressed");
             cam.transform.position = new Vector3(currPos.x , currPos.y - this.moveSpeed, currPos.z);
 
