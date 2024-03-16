5c4302d4f4f7ec3b8a654fc96f0dd75da28292e3
rony1philip <26e4u69@gmail.com>
2024-03-16 03:31:06 +0200

    shield state machine

--- Assets/Scripts/wasd_move_cam.cs
+++ Assets/Scripts/wasd_move_cam.cs
@@ -19,31 +19,25 @@ public class wasd_move_cam : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(new Vector3(0,0,1) * (rotationSpeed * Time.deltaTime));
-            //Debug.Log("<-");
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(new Vector3(0,0,-1) * (rotationSpeed * Time.deltaTime));
-            //Debug.Log("->");
         }
         if (Input.GetKey(KeyCode.A)) 
         {
-            //Debug.Log("A pressed");
             transform.Translate(new Vector3(-1,0,0) * (moveSpeed * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.D)) 
         {
-            //Debug.Log("D pressed");
             transform.Translate(new Vector3(1,0,0) * (moveSpeed * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.W)) 
         {
-            //Debug.Log("W pressed");
             transform.Translate(new Vector3(0,1,0) * (moveSpeed * Time.deltaTime));
         }
         if (Input.GetKey(KeyCode.S)) 
         {
-            //Debug.Log("S pressed");
             transform.Translate(new Vector3(0,-1,0) * (moveSpeed * Time.deltaTime));
         }
 
