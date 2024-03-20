b0c16884dc0785f95c10de5aa8f4cec15dbf58dd
rony1philip <26e4u69@gmail.com>
2024-03-20 15:18:44 +0200

    Fixed state machine Started throw mechanic Started equip mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private GameManager _gameManager;
+    private ShieldBehaviour _shieldBehaviour;
     
     protected void Start()
     {
         _gameManager = GameManager.Instance.GetComponent<GameManager>();
+        _shieldBehaviour = GameObject.FindGameObjectWithTag("Shield").GetComponent<ShieldBehaviour>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("COLLISION");
-        DestroyPlayer();
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            DestroyPlayer();
+        }
     }
 
     private void DestroyPlayer()
